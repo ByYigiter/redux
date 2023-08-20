83e5046c15ba6d8ce9eefd1bacfa05e3fae7a2a1
Yigit Er <mr.yigiter222@gmail.com>
2023-08-20 15:53:50 +0300

    axios

---
+++ src/App2.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import "./App2.css";
+import axios from "axios";
+
+const App2 = () => {
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		const fetchTodos = async () => {
+			try {
+				const response = await axios.get(
+					"https://jsonplaceholder.typicode.com/todos/"
+				);
+				setTodos(response.data);
+			} catch (err) {
+				console.log("Error", err);
+			}
+		};
+		fetchTodos();
+	}, []);
+
+	return (
+		<div>
+			<div className="todo-wrapper"></div>
+			{todos.map((todo) => {
+				return (
+					<div key={todo.id} className="todo">
+						<h3>{todo.title}</h3>
+						<input
+							type="checkbox"
+							checked={todo.completed}
+							className="todo-completed"
+						/>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
+
+export default App2;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './modules/store';
+import App2 from './App2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}>
     <App />
-    </Provider>
+    </Provider> */}
+
+    <App2/>
   </React.StrictMode>
 );
 
